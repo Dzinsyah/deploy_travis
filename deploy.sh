56e3b6549337294722a37a16f6856be0e2c9135c
Dzinsyah <dzinsyah@alphatech.id>
2019-03-20 15:56:28 +0700

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 eval "$(ssh-agent -s)" &&
 ssh-add -k ~/.ssh/id_rsa &&
-cd /home/ubuntu/deploy/project_e_commerce
+cd /home/ubuntu/travis/deploy_travis
 git pull
 
 source ~/.profile
 echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
 sudo docker stop e_commerce
 sudo docker rm e_commerce
-sudo docker rmi dzinsyah/e_commerce
+sudo docker rmi dzinsyah/e_commerce:latest
 sudo docker run  -d --name e_commerce -p 8000:8000 -t dzinsyah/e_commerce:latest
